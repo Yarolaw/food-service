f11dd21de36f4331002955bfe2d27d8796a53bb7
Yaroslaw Dmukhovsckyi <ermacus096@gmail.com>
2020-12-07 23:31:17 +0200

    Руфакторинг кода

--- src/js/switch.js
+++ src/js/switch.js
@@ -1,36 +1,34 @@
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
 const bodyRef = document.querySelector("body")
 const inputRef = document.querySelector("#theme-switch-toggle")
 
 const checkTheme = () => {
-    if (localStorage.getItem('theme') === 'dark-theme') {
+    if (localStorage.getItem('theme') === Theme.DARK) {
         inputRef.checked = true;
-        bodyRef.classList.add('dark-theme');
+        bodyRef.classList.add(Theme.DARK);
     } else {
-        bodyRef.classList.add('light-theme');
+        bodyRef.classList.add(Theme.LIGHT);
     }
 }
 checkTheme();
 
 const changeTheme = () => {
-    if (inputRef.checked === false) {
-        bodyRef.classList.remove("dark-theme")
-        bodyRef.classList.add("light-theme")
-        localStorage.setItem("theme", "light-theme")
-        
+    if (!inputRef.checked) {
+        localStorage.setItem("theme", Theme.LIGHT)
+        bodyRef.classList.remove(Theme.DARK)
+        bodyRef.classList.add(Theme.LIGHT)
+
     }
-    else {
-        bodyRef.classList.remove("light-theme")
-        bodyRef.classList.add("dark-theme")
-        localStorage.setItem("theme", "dark-theme")
+    if (inputRef.checked) {
+        localStorage.setItem("theme", Theme.DARK)
+         bodyRef.classList.remove(Theme.LIGHT)
+        bodyRef.classList.add(Theme.DARK)
         
     }
 }
-    
-
-
-
-
-
 
 inputRef.addEventListener('change', changeTheme)
-console.dir(inputRef)
